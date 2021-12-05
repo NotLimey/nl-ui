681e29c6838bee5447615b963964a6d7eb83adf4
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2021-12-05 02:43:37 +0100

    Fixed title helmet

--- src/components/Helmets/TitleHelmet.tsx
+++ src/components/Helmets/TitleHelmet.tsx
@@ -7,14 +7,14 @@ const TitleHelmet = ({Title, SubTitle} : any) => {
     return (
         <Helmet>
             {/* Primary Meta Tags */}
-            <title>{Title ? Title : `${SubTitle ? ` | ${SubTitle}` : ""}`}</title>
-            <meta name="title" content={Title ? Title : `${SubTitle ? ` | ${SubTitle}` : ""}`} />
-            <meta name="application-name" content={SubTitle ? `${Title ? Title : ''} | ${SubTitle}` : `${Title}`} />
-            <meta property="og:title" content={Title ? Title : `${SubTitle ? ` | ${SubTitle}` : ""}`} />
-            <meta property="twitter:title" content={Title ? Title : `${SubTitle ? ` | ${SubTitle}` : ""}`} />
+            <title>{`${Title}${SubTitle ? ` | ${SubTitle}` : ''}`}</title>
+            <meta name="title" content={`${Title}${SubTitle ? ` | ${SubTitle}` : ''}`} />
+            <meta name="application-name" content={`${Title}${SubTitle ? ` | ${SubTitle}` : ''}`} />
+            <meta property="og:title" content={`${Title}${SubTitle ? ` | ${SubTitle}` : ''}`} />
+            <meta property="twitter:title" content={`${Title}${SubTitle ? ` | ${SubTitle}` : ''}`} />
 
             {/* Apple */}
-            <meta name="apple-mobile-web-app-title" content={Title ? Title : `${SubTitle ? ` | ${SubTitle}` : ""}`} />
+            <meta name="apple-mobile-web-app-title" content={`${Title}${SubTitle ? ` | ${SubTitle}` : ''}`} />
         </Helmet>
     );
 }
