b7781fd0d455af74d69947bc89b20eb4bf09dbfa
Martin Myhre <70258183+martinlime@users.noreply.github.com>
2022-02-25 12:00:32 +0100

    Default values

--- src/components/Seo/DefaultHelmet.tsx
+++ src/components/Seo/DefaultHelmet.tsx
@@ -1,26 +1,61 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { Helmet } from "react-helmet";
 
-let defaultTitle = "";
+var defaultSettings : any = {}
 
 interface DefaultHelmetProps {
     title?: string;
-    defaultValue?: boolean;
+    subTitle?: string;
+    titleDivider?: string;
 }
 
-export const DefaultHelmet : React.FC<DefaultHelmetProps> = (props: DefaultHelmetProps) => {
+export const DefaultHelmet = (props : DefaultHelmetProps) => {
 
-    useEffect(() => {
-    }, [])
+    const _defaultSettings = defaultSettings;
+
+    if(props) {
+        const keys = Object.keys(props);
+        for(var i = 0; i < keys.length; i++) {
+            if(props[keys[i]])
+                defaultSettings[keys[i]] = props[keys[i]]
+        }
+    }
+
+    const getTitle = () => {
+        let text = "";
+
+        if(props.title) {
+            text = text + props.title;
+        }else {
+            if(_defaultSettings.title) {
+                text = text + _defaultSettings.title;
+            }
+        }
+
+        
+        if(props.subTitle || _defaultSettings.subTitle) {
+            let subTtl = props.subTitle ?? _defaultSettings.subTitle;
+
+            if(props.titleDivider || _defaultSettings.titleDivider) {
+                text = text + ` ${props.titleDivider ?? _defaultSettings.titleDivider} ` + subTtl
+            }else {
+                text = text + " | " + subTtl
+            }
+        }
+
+        return text
+    }
 
     return (
-        <div>
-
-        </div>
-    );
+        <React.Fragment>
+            {(props.title || _defaultSettings.title) &&
+                <Helmet>
+                    <title>
+                        {getTitle()}
+                    </title>
+                    <meta name="og:title" content="The Rock"/>
+                </Helmet>
+            }
+        </React.Fragment>
+    )
 }
-
-/*
-
-    DefaultHelmet
-
-*/
